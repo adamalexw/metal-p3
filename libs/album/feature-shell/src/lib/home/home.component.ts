d89d279cd9e87e27e839b2edad8a163b1e2a7343
Adam Warren <adamalexw05@gmail.com>
2024-02-29 07:41:36 +1100

    chore: nx upgrade

--- libs/album/feature-shell/src/lib/home/home.component.ts
+++ libs/album/feature-shell/src/lib/home/home.component.ts
@@ -1,7 +1,7 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AsyncPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, ViewChild, inject } from '@angular/core';
-import { MatDrawerMode, MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatDrawerMode, MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { AlbumActions, selectAdvancedSearchOpen, selectSideNavOpen } from '@metal-p3/shared/data-access';
 import { Store } from '@ngrx/store';
@@ -19,9 +19,6 @@ import { ListComponent } from '../list/list.component';
 export class HomeComponent {
   private readonly store = inject(Store);
   private readonly breakpointObserver = inject(BreakpointObserver);
-
-  @ViewChild('searchnav') searchNav!: MatSidenav;
-  @ViewChild('sidenav') sideNav!: MatSidenav;
 
   avancedSearchOpen$ = this.store.select(selectAdvancedSearchOpen);
   sideNavOpen$ = this.store.select(selectSideNavOpen);
