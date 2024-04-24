465811b93fbead5e40b25903e7e7d1153cc1ed59
Adam Warren <adamalexw05@gmail.com>
2024-04-25 09:30:47 +1000

    feat: no more cloud flare

--- libs/shared/metal-archives/src/lib/metal-archives-cloud-flare.service.ts
+++ libs/shared/metal-archives/src/lib/metal-archives-cloud-flare.service.ts
@@ -1,24 +1,31 @@
 import type { BandProps, MetalArchivesAlbumTrack, MetalArchivesSearchResponse, MetalArchivesSearchResponseItem } from '@metal-p3/api-interfaces';
 import { extractUrl } from '@metal-p3/shared/utils';
-import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { parse } from 'node-html-parser';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import puppeteer from 'puppeteer-extra';
+// import createPuppeteerStealth from 'puppeteer-extra-plugin-stealth';
+import { Observable, catchError, from, map, of } from 'rxjs';
 
 @Injectable()
 export class MetalArchivesService {
   readonly baseUrl = 'https://www.metal-archives.com/';
 
-  constructor(private readonly httpService: HttpService) {}
-
   findUrl(artist: string, album: string): Observable<MetalArchivesSearchResponse> {
-    return this.httpService
-      .get<MetalArchivesSearchResponse>(`${this.baseUrl}search/ajax-advanced/searching/albums/?bandName=${encodeURIComponent(artist)}&releaseTitle=${encodeURIComponent(album)}`)
-      .pipe(
-        map((response) => response.data),
-        map((response) => ({ ...response, results: this.mapSearchResults(response.aaData) })),
-      );
+    return this.getApiResponse<MetalArchivesSearchResponse>(
+      `${this.baseUrl}search/ajax-advanced/searching/albums/?bandName=${encodeURIComponent(artist)}&releaseTitle=${encodeURIComponent(album)}`,
+      'iTotalRecords',
+      true,
+    ).pipe(
+      map((response: MetalArchivesSearchResponse) => ({ ...response, results: this.mapSearchResults(response.aaData) })),
+      catchError((error) => {
+        console.log(error);
+        return of({
+          iTotalRecords: 0,
+          aaData: [],
+          results: [],
+        });
+      }),
+    );
   }
 
   private mapSearchResults(
@@ -85,11 +92,61 @@ export class MetalArchivesService {
   }
 
   getLyrics(trackId: string): Observable<string> {
-    return this.httpService.get<string>(`${this.baseUrl}release/ajax-view-lyrics/id/${trackId}`).pipe(map((response) => response.data.trim()));
+    return this.getApiResponse<string>(`${this.baseUrl}release/ajax-view-lyrics/id/${trackId}`, '<br>').pipe(map((htmlLyrics) => parse(htmlLyrics).innerText));
+  }
+
+  private getApiResponse<T>(url: string, waitItem: string, isJson = false): Observable<T | string> {
+    return from(this.getPageContents<T>(url, 'url', waitItem, isJson)).pipe(
+      catchError((err) => {
+        console.log(err);
+        return of('');
+      }),
+    );
   }
 
   private downloadPage(url: string): Observable<string> {
-    return this.httpService.get<string>(url).pipe(map((response) => response.data));
+    return from(this.getPageContents<string>(url, 'page', '#MA_logo')).pipe(
+      catchError((err) => {
+        console.log(err);
+        return of('');
+      }),
+    );
+  }
+
+  private async getPageContents<T>(url: string, waitFor: 'page' | 'url', waitItem: string, isJson = false): Promise<string | T> {
+    const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+    const puppeteerStealth = StealthPlugin();
+
+    // const puppeteerStealth = createPuppeteerStealth();
+    puppeteerStealth.enabledEvasions.delete('user-agent-override');
+    puppeteer.use(puppeteerStealth);
+
+    const browser = await puppeteer.launch({
+      headless: false,
+      targetFilter: (target) => target.type() !== 'other',
+    });
+    const defaultPage = (await browser.pages())[0]; // <-- bypasses Cloudflare
+
+    await defaultPage.goto(url);
+
+    if (waitFor === 'url') {
+      await defaultPage.waitForFunction(`document.querySelector("body").innerHTML.includes("${waitItem}")`);
+      const content = await defaultPage.content();
+
+      if (!isJson) {
+        return content;
+      }
+
+      const apiResponse = this.extractApiResponse(content);
+
+      return (await JSON.parse(apiResponse)) as T;
+    }
+
+    await defaultPage.waitForSelector(waitItem, { timeout: 10000 }).catch((error) => console.error(error));
+    const content = await defaultPage.content();
+
+    //await browser.close();
+    return content;
   }
 
   getBandProps(url: string): Observable<BandProps> {
@@ -112,5 +169,17 @@ export class MetalArchivesService {
     props.genre = row2.querySelector('dd').textContent.replace(/\//g, ' - ');
 
     return props;
+  }
+
+  private extractApiResponse(content: string) {
+    const start = content.indexOf('{');
+    const end = content.indexOf('}');
+
+    const json = content
+      .substring(start, end + 1)
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>');
+
+    return json;
   }
 }
