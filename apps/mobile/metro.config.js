091eaf2bb070cebe6c8a428c21ebfe4ef9ab92db
Adam Warren <adam.warren@ratemyagent.com>
2026-05-30 09:47:10 +1000

    app compliling

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -24,7 +24,7 @@ const customConfig = {
   },
 };
 
-module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
+const finalConfig = withNxMetro(mergeConfig(defaultConfig, customConfig), {
   // Change this to true to see debugging info.
   // Useful if you have issues resolving modules
   debug: false,
@@ -33,3 +33,16 @@ module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
   // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
   watchFolders: [],
 });
+
+const previousRewrite = finalConfig.server?.rewriteRequestUrl;
+finalConfig.server = finalConfig.server || {};
+finalConfig.server.rewriteRequestUrl = (url) => {
+  const rewritten = previousRewrite ? previousRewrite(url) : url;
+  if (!rewritten.includes('transform.routerRoot=')) {
+    const sep = rewritten.includes('?') ? '&' : '?';
+    return rewritten + sep + 'transform.routerRoot=' + encodeURIComponent('apps/mobile/app');
+  }
+  return rewritten;
+};
+
+module.exports = finalConfig;
